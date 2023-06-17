cc1479625fe36f7184b4c22a7b476d6b4ff78660
Amulya Parab <amulyaparab101@gmail.com>
2023-06-17 22:13:16 +0530

    docs: removed footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./Components/Navbar";
 import { BookShelf } from "./Pages/BookShelf";
 import { Search } from "./Pages/Search";
-import { Footer } from "./Components/Footer";
 
 function App() {
   return (
@@ -15,7 +13,6 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="*" element={<h1>Page Not Found.</h1>} />
       </Routes>
-      <Footer />
     </div>
   );
 }
